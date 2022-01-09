abfaded76537670560ad1982a59e5cc89b32f3ab
Hunter <88465412+Hunyeh@users.noreply.github.com>
2022-01-09 17:27:49 -0500

    contact section added/ tested

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,6 +6,8 @@ import Nav from '..';
 const categories = [{ name: 'portraits', description: 'Portraits of people in my life' }]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -16,6 +18,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
         />);
     })
     // snapshot test
